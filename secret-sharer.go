0b2c4c38812a118804c5eafed0fbc364cd8c1f1e
Allen Garvey <allengarvey@hotmail.com>
2024-07-20 17:21:54 -0400

    Add ability to store and recall secrets

--- secret-sharer.go
+++ secret-sharer.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
-	"log"
 	"github.com/gin-gonic/gin"
+	"log"
 	"math/rand/v2"
+	"net/http"
+	"strconv"
 )
 
 type Secret struct {
@@ -15,14 +16,15 @@ type Secret struct {
 
 func main() {
 	secrets := map[int]Secret{}
-	
+	const SITE_TITLE = "Secret Sharer"
+
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 	router.Static("/assets", "./assets")
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title": "Secret Sharer",
+			"title": SITE_TITLE,
 		})
 	})
 
@@ -37,10 +39,33 @@ func main() {
 		secrets[key] = createParams
 
 		c.HTML(http.StatusOK, "created.tmpl", gin.H{
-			"title": "Secret Sharer",
+			"title":   SITE_TITLE,
 			"itemUrl": fmt.Sprintf("/items/%d", key),
 		})
 	})
-	
+
+	router.GET("/items/:key", func(c *gin.Context) {
+		keyParam := c.Param("key")
+
+		key, err := strconv.Atoi(keyParam)
+		if err != nil {
+			c.String(http.StatusNotFound, "Not found")
+			return
+		}
+
+		secret, keyExists := secrets[key]
+
+		if !keyExists {
+			c.String(http.StatusNotFound, "Not found")
+			return
+		}
+
+		c.HTML(http.StatusOK, "show.tmpl", gin.H{
+			"title":       SITE_TITLE,
+			"itemTitle":   secret.Title,
+			"itemContent": secret.Content,
+		})
+	})
+
 	router.Run(":3000")
 }
